92c64b12c9f123ebaa6128958d17e0e3933123d1
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-07-21 21:24:51 -0400

    V1.1 Update
    Tons of additions!
    - Fixed hoppers not transferring to other hoppers at the correct speed
    - Now allows multiple items to be transferred at a time
    - Safer config
    - Commands are redone to tell admins when something is changed
    - Code cleanups

--- src/main/java/io/github/qpcrummer/hopperspeedsimulator/commands/Command.java
+++ src/main/java/io/github/qpcrummer/hopperspeedsimulator/commands/Command.java
@@ -4,16 +4,14 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
-import static io.github.qpcrummer.hopperspeedsimulator.Main.properties;
-import static io.github.qpcrummer.hopperspeedsimulator.Main.ticks;
+import static io.github.qpcrummer.hopperspeedsimulator.Main.*;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -23,38 +21,47 @@ public class Command {
     public static void registerCommand() throws FileNotFoundException {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> dispatcher.register(CommandManager.literal("hopperspeed")
                         .executes(context -> {
-                            ServerPlayerEntity player = context.getSource().getPlayer();
-                            assert player != null;
-                            player.sendMessage(Text.of("Current speed is "+ ticks +" ticks per transfer"));
+                            context.getSource().sendFeedback(Text.of("Current speed is "+ ticks +" ticks per transfer"), true);
                             return 1;
                         })
-                .then(argument("ticks-per-item", IntegerArgumentType.integer()) .executes(context -> {
-                    int ticks2 = IntegerArgumentType.getInteger(context, "ticks-per-item");
-                    properties.setProperty("ticks-per-transfer", String.valueOf(ticks2));
-                    try (OutputStream output = new FileOutputStream(String.valueOf(FabricLoader.getInstance().getConfigDir().resolve("hopperspeedsim.properties")))) {
+
+                .then(literal("ticks-per-item").then(argument("ticks-per-item", IntegerArgumentType.integer()).executes(context -> {
+                    properties.setProperty("ticks-per-transfer", String.valueOf(IntegerArgumentType.getInteger(context, "ticks-per-item")));
+                    try (OutputStream output = Files.newOutputStream(FabricLoader.getInstance().getConfigDir().resolve("hopperspeedsim.properties"))) {
                         properties.store(output, null);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     ticks = Integer.parseInt(properties.getProperty("ticks-per-transfer"));
-                    ServerPlayerEntity player = context.getSource().getPlayer();
-                            assert player != null;
-                            player.sendMessage(Text.of("Hoppers will now transfer items every "+ ticks +" ticks"));
+                    context.getSource().sendFeedback(Text.of("Hoppers will now transfer items every "+ ticks +" ticks"), true);
                     return 1;
-                }))
+                })))
+
                 .then(literal("default").executes(context -> {
                     properties.setProperty("ticks-per-transfer", "8");
-                    try (OutputStream output = new FileOutputStream(String.valueOf(FabricLoader.getInstance().getConfigDir().resolve("hopperspeedsim.properties")))) {
+                    properties.setProperty("items-per-transfer", "1");
+                    try (OutputStream output = Files.newOutputStream(FabricLoader.getInstance().getConfigDir().resolve("hopperspeedsim.properties"))) {
                         properties.store(output, null);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     ticks = Integer.parseInt(properties.getProperty("ticks-per-transfer"));
-                    ServerPlayerEntity player = context.getSource().getPlayer();
-                           assert player != null;
-                           player.sendMessage(Text.of("Hoppers will now transfer normally at 8 ticks per item"));
+                    items = Integer.parseInt(properties.getProperty("items-per-transfer"));
+                    context.getSource().sendFeedback(Text.of("Hoppers will now transfer normally at 1 item per 8 ticks"), true);
                     return 1;
-                        }
-                ))));
+                }))
+
+                .then(literal("items-per-transfer").then(argument("items-per-transfer", IntegerArgumentType.integer()).executes(context -> {
+                    properties.setProperty("items-per-transfer", String.valueOf(IntegerArgumentType.getInteger(context, "items-per-transfer")));
+                            try (OutputStream output = Files.newOutputStream(FabricLoader.getInstance().getConfigDir().resolve("hopperspeedsim.properties"))) {
+                                properties.store(output, null);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            items = Integer.parseInt(properties.getProperty("items-per-transfer"));
+                            context.getSource().sendFeedback(Text.of("Hoppers will now move " + items + " items per transfer"), true);
+                    return 1;
+                }
+                )))));
     }
 }
